b824132e20bd5b82ffad3574066833ffadd171f8
Juan Luis Casanova <juanluiscr27@hotmail.com>
2023-02-20 20:00:31 -0500

    Commented database sql files

---
+++ src/main/resources/data.sql
@@ -0,0 +1,7 @@
+ -- CSD 4464 – Programming Java EE
+ -- Term Project - Airline Reservation System
+ --
+ -- Host: 127.0.0.1    Database: Flyme
+ -- ------------------------------------------------------
+ -- MySQL Server version	8.0.27
+
