2f4c41aaa65f96209143e9d157cd8cb73a286373
Juan Luis Casanova <juanluiscr27@hotmail.com>
2023-02-28 22:59:44 -0500

    Added servlet class to forward requests to the signup page

---
+++ src/main/java/controller/SignUpServlet.java
@@ -0,0 +1,26 @@
+package controller;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.Serial;
+
+@WebServlet("/signup")
+public class SignUpServlet extends HttpServlet {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * Forwards the request to the Sign-Up HTML page
+     * @param request HTTP Request
+     * @param response HTTP Response
+     */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        RequestDispatcher requestDispatcher = request.getRequestDispatcher("/html/signup.html");
+    }
+}
