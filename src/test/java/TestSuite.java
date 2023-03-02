510be53737511a504ca2b4102df279a0b5b3110a
Juan Luis Casanova <juanluiscr27@hotmail.com>
2023-03-01 21:28:46 -0500

    Added a Test class for the UserDAO methods

--- src/test/java/TestSuite.java
+++ src/test/java/TestSuite.java
@@ -3,12 +3,14 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import repository.DatabaseConnectionPoolTest;
 import repository.PropertiesFileReaderTest;
+import repository.UserDAOTest;
 
 @RunWith(Suite.class)
 
 @Suite.SuiteClasses({
         PropertiesFileReaderTest.class,
         DatabaseConnectionPoolTest.class,
+        UserDAOTest.class,
 })
 
 public class TestSuite {
