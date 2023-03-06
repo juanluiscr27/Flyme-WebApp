560c03c2e612c8c29b84bd8179f34bf4f10b34d8
Juan Luis Casanova <juanluiscr27@hotmail.com>
2023-03-05 20:20:30 -0500

    Added Login Servlet

--- src/main/java/service/UserService.java
+++ src/main/java/service/UserService.java
@@ -4,6 +4,8 @@ import model.User;
 import repository.UserRepository;
 import util.PasswordEncoder;
 
+import java.util.Optional;
+
 public class UserService {
     private final UserRepository userRepo;
 
@@ -15,5 +17,13 @@ public class UserService {
         String encodedPassword = PasswordEncoder.encodePassword(registrationRequest.getPassword());
         registrationRequest.setPassword(encodedPassword);
         return userRepo.add(registrationRequest);
+    }
+    public User login(String email, String password) throws IllegalArgumentException {
+        Optional<User> unvalidatedUser = userRepo.findByEmail(email);
+        User verifiedUser = unvalidatedUser.orElseThrow(() -> new IllegalArgumentException("User not found"));
+        String encodedPassword = PasswordEncoder.encodePassword(password);
+        if (!encodedPassword.equals(verifiedUser.getPassword()))
+            throw new IllegalArgumentException("Incorrect password");
+        return verifiedUser;
     }
 }
