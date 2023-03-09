ff6a327f83e45ea7da198a618b614498d31cc47c
Juan Luis Casanova <juanluiscr27@hotmail.com>
2023-03-08 22:40:02 -0500

    Added Payment Entity class

---
+++ src/main/java/model/Payment.java
@@ -0,0 +1,73 @@
+package model;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+
+public class Payment implements Serializable {
+    private long id;
+    private String cardNumber;
+    private String nameOnCard;
+    private LocalDate expiryDate;
+    private int securityCode;
+
+    public Payment(String cardNumber, String nameOnCard, LocalDate expiryDate, int securityCode) {
+        this.cardNumber = cardNumber;
+        this.nameOnCard = nameOnCard;
+        this.expiryDate = expiryDate;
+        this.securityCode = securityCode;
+    }
+    public Payment(long paymentId, String cardNumber, String nameOnCard, LocalDate expiryDate, int securityCode) {
+        this.id = paymentId;
+        this.cardNumber = cardNumber;
+        this.nameOnCard = nameOnCard;
+        this.expiryDate = expiryDate;
+        this.securityCode = securityCode;
+    }
+    public long getId() {
+        return id;
+    }
+    public void setId(long id) {
+        this.id = id;
+    }
+    public String getCardNumber() {
+        return cardNumber;
+    }
+
+    public void setCardNumber(String cardNumber) {
+        this.cardNumber = cardNumber;
+    }
+
+    public String getNameOnCard() {
+        return nameOnCard;
+    }
+
+    public void setNameOnCard(String nameOnCard) {
+        this.nameOnCard = nameOnCard;
+    }
+
+    public LocalDate getExpiryDate() {
+        return expiryDate;
+    }
+
+    public void setExpiryDate(LocalDate expiryDate) {
+        this.expiryDate = expiryDate;
+    }
+
+    public int getSecurityCode() {
+        return securityCode;
+    }
+
+    public void setSecurityCode(int securityCode) {
+        this.securityCode = securityCode;
+    }
+
+    @Override
+    public String toString() {
+        return "Payment {" +
+                "cardNumber='" + cardNumber + '\'' +
+                ", nameOnCard='" + nameOnCard + '\'' +
+                ", expiryDate=" + expiryDate +
+                ", securityCode=" + securityCode +
+                '}';
+    }
+}
