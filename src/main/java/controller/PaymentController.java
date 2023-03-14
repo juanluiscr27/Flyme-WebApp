c406a1d59311e2aeed410de72f12913e1b4274b9
Juan Luis Casanova <juanluiscr27@hotmail.com>
2023-03-13 23:43:01 -0400

    Modified payment controller. Added update payment method

--- src/main/java/controller/PaymentController.java
+++ src/main/java/controller/PaymentController.java
@@ -47,6 +47,28 @@ public class PaymentController extends HttpServlet {
 
         HttpSession session = request.getSession();
         User user = (User) session.getAttribute("user");
+
+        PaymentRepository paymentRepo = new PaymentDAO();
+        PaymentService paymentService = new PaymentService(paymentRepo);
+
+        Payment newPayment = new Payment(
+                request.getParameter("card-number"),
+                request.getParameter("name-on-card"),
+                LocalDate.parse(request.getParameter("expiry-date")),
+                Integer.parseInt(request.getParameter("security-code")),
+                user.getId()
+        );
+
+        Payment payment = paymentService.add(newPayment);
+
+        RequestDispatcher requestDispatcher = request.getRequestDispatcher(StaticPage.LOGIN.path);
+        requestDispatcher.forward(request, response);
+    }
+    protected void doPut(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        HttpSession session = request.getSession();
+        User user = (User) session.getAttribute("user");
         int paymentId = Integer.parseInt(request.getParameter("payment-id"));
 
         PaymentRepository paymentRepo = new PaymentDAO();
@@ -61,16 +83,11 @@ public class PaymentController extends HttpServlet {
                 user.getId()
         );
 
-        if (paymentId < 0 ) {
-            Payment payment = paymentService.add(newPayment);
-        } {
-            Payment payment = paymentService.update(newPayment);
-        }
+        Payment payment = paymentService.update(newPayment);
 
         RequestDispatcher requestDispatcher = request.getRequestDispatcher(StaticPage.LOGIN.path);
         requestDispatcher.forward(request, response);
     }
-
     protected void doDelete(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
 
