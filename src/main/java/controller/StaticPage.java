fbbc6fe0ca2875a20e7c310848b338ba9ed46226
Juan Luis Casanova <juanluiscr27@hotmail.com>
2023-03-17 20:54:15 -0400

    Added new static html pages

--- src/main/java/controller/StaticPage.java
+++ src/main/java/controller/StaticPage.java
@@ -5,7 +5,12 @@ public enum StaticPage {
     SIGNUP("/html/signup.html"),
     LOGIN("/html/login.html"),
     PROFILE("/html/profile.html"),
-    SEARCH("/html/search.html");
+    SEARCH("/html/search.html"),
+    FLIGHT("/html/flight.html"),
+    PASSENGERS("/html/passengers.html"),
+    SEATS("/html/seats.html"),
+    SUMMARY("/html/summary.html"),
+    PAY("/html/pay.html");
 
     final String path;
 
