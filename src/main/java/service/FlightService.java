eaee5575a2828c7707cba92f5fbd89085de61c40
Juan Luis Casanova <juanluiscr27@hotmail.com>
2023-03-19 09:32:36 -0400

    Implemented search methods to one way flights and round trip on Service class

--- src/main/java/service/FlightService.java
+++ src/main/java/service/FlightService.java
@@ -1,4 +1,22 @@
 package service;
 
+import model.Flight;
+import model.FlightSearchDTO;
+import repository.FlightRepository;
+
+import java.util.List;
+
 public class FlightService {
+
+    FlightRepository flightRepo;
+    public FlightService(FlightRepository flightRepo) {
+        this.flightRepo = flightRepo;
+    }
+
+    public List<Flight> findAllOneWay(FlightSearchDTO flightSearch) {
+        return flightRepo.findAllWithSearchCriteria(flightSearch, flightSearch.departureDate());
+    }
+    public List<Flight> findAllRoundTrip(FlightSearchDTO flightSearch) {
+        return flightRepo.findAllWithSearchCriteria(flightSearch, flightSearch.returnDate());
+    }
 }
