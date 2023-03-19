acc4a450e0e28446f549e6069945347825539da4
Juan Luis Casanova <juanluiscr27@hotmail.com>
2023-03-19 09:23:18 -0400

    Refactor Flight Repository to include a criteria base search for flights

--- src/main/java/repository/FlightRepository.java
+++ src/main/java/repository/FlightRepository.java
@@ -2,7 +2,9 @@ package repository;
 
 import model.CountryDTO;
 import model.Flight;
+import model.FlightSearchDTO;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
@@ -11,6 +13,9 @@ public interface FlightRepository {
     Optional<Flight> find(Long id);
 
     List<Flight> findAll();
+
+    List<Flight> findAllWithSearchCriteria(FlightSearchDTO flightSearch, LocalDate travelDate);
+
 
     List<Flight> findAllByCountry(CountryDTO countryDTO);
 }
