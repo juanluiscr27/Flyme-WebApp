7db45f7e1030ab83b0cb29ce6609e6318603b2a4
Juan Luis Casanova <juanluiscr27@hotmail.com>
2023-03-19 09:34:11 -0400

    Implemented GET method on Flight Servlet

--- src/main/java/controller/FlightServlet.java
+++ src/main/java/controller/FlightServlet.java
@@ -1,13 +1,23 @@
 package controller;
 
+import model.Flight;
+import model.FlightSearchDTO;
+import model.Reservation;
+import repository.FlightDAO;
+import repository.FlightRepository;
+import service.FlightService;
+
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.io.Serial;
+import java.time.LocalDate;
+import java.util.List;
 
 @WebServlet("/flights")
 public class FlightServlet extends HttpServlet {
@@ -15,6 +25,27 @@ public class FlightServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+
+        FlightRepository flightRepo = new FlightDAO();
+        FlightService flightService = new FlightService(flightRepo);
+
+        FlightSearchDTO flightSearch = new FlightSearchDTO(
+                request.getParameter("from"),
+                request.getParameter("to"),
+                LocalDate.parse(request.getParameter("departure")),
+                request.getParameter("return").equals("") ? null : LocalDate.parse(request.getParameter("return")),
+                Integer.parseInt(request.getParameter("passengers"))
+        );
+
+        Boolean isRoundTrip = Boolean.valueOf(request.getParameter("round-trip"));
+        Reservation reservation = new Reservation();
+
+        List<Flight> allFlights = flightService.findAllOneWay(flightSearch);
+
+        System.out.println(allFlights);
+        HttpSession session = request.getSession();
+        session.setAttribute("allFlights", allFlights);
+        session.setAttribute("reservation", reservation);
 
         RequestDispatcher requestDispatcher = request.getRequestDispatcher(StaticPage.FLIGHT.path);
         requestDispatcher.forward(request, response);
