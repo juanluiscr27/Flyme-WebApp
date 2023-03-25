df78c686049caa237fecd66f171563fcf9bb26ce
Juan Luis Casanova <juanluiscr27@hotmail.com>
2023-03-25 00:40:42 -0400

    Modified Flight Repository interface to use Map instead of List

--- src/main/java/repository/FlightDAO.java
+++ src/main/java/repository/FlightDAO.java
@@ -34,15 +34,34 @@ public class FlightDAO implements FlightRepository {
         List<Flight> allFlights = new ArrayList<>();
         try {
             statement = connection.prepareStatement("SELECT " +
-                    "flight_id, flight_number, origin, destination, plane_id, departure, arrival " +
-                    "FROM flights " +
+                    "f.flight_id, f.flight_number, f.plane_id, p.registration, p.manufacturer, p.model, p.price_multiplier, " +
+                    "f.departure, f.origin, oa.name, oa.city, oa.country, oc.country_name, oa.latitude, oa.longitude, " +
+                    "f.arrival, f.destination, da.name, da.city, da.country, dc.country_name, da.latitude, da.longitude, " +
+                    "c.name, COUNT( s.seat_id ) AS seat , ( " +
+                    "    SELECT COUNT(sqp.passenger_id) AS passenger " +
+                    "    FROM passengers AS sqp " +
+                    "    INNER JOIN seats AS sqs ON sqs.seat_id = sqp.seat_id " +
+                    "    LEFT JOIN classes AS sqc ON sqc.class_id = sqs.class_id " +
+                    "    WHERE sqc.class_id = c.class_id AND sqp.flight_id = f.flight_id " +
+                    "  ) AS passenger " +
+                    "FROM flights AS f " +
+                    "INNER JOIN seats AS s ON s.plane_id = f.plane_id " +
+                    "INNER JOIN classes AS c ON c.class_id = s.class_id " +
+                    "INNER JOIN planes AS p ON f.plane_id = p.plane_id " +
+                    "INNER JOIN airports AS oa ON f.origin = oa.airport_id " +
+                    "INNER JOIN countries AS oc ON oa.country = oc.country_id " +
+                    "INNER JOIN airports AS da ON f.destination = da.airport_id " +
+                    "INNER JOIN countries AS dc ON da.country = dc.country_id " +
                     "WHERE origin = ? AND destination = ? " +
-                    "AND departure BETWEEN ? AND ? ");
+                    "AND departure BETWEEN ? AND ? " +
+                    "GROUP BY f.flight_id, f.flight_number, f.plane_id, c.name, passenger " +
+                    "HAVING seat - passenger >= ? ");
 
             statement.setString(1, flightSearch.originAirport());
             statement.setString(2, flightSearch.destinationAirport());
             statement.setTimestamp(3, Timestamp.valueOf(travelDate.atStartOfDay()));
             statement.setTimestamp(4, Timestamp.valueOf(travelDate.atTime(LocalTime.MAX)));
+            statement.setInt(5, flightSearch.passengers());
 
             resultSet = statement.executeQuery();
 
