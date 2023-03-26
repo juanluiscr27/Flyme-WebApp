05b24b79546990004661a6535b2664a94d2a4085
Juan Luis Casanova <juanluiscr27@hotmail.com>
2023-03-26 01:25:57 -0400

    Added a DTO for identifying the class for the seats

---
+++ src/main/java/model/SeatClassDTO.java
@@ -0,0 +1,11 @@
+package model;
+
+import java.math.BigDecimal;
+
+public record SeatClassDTO(
+        int classId,
+        String className,
+        int checkedBags,
+        BigDecimal priceMultiplier
+) {
+}
