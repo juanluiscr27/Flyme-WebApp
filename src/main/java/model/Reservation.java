423e02db1df027041b20397bf53b832bde6a6ccb
Juan Luis Casanova <juanluiscr27@hotmail.com>
2023-04-02 13:18:41 -0400

    Added required methods to reservation entity

--- src/main/java/model/Reservation.java
+++ src/main/java/model/Reservation.java
@@ -1,5 +1,16 @@
 package model;
 
+import java.util.List;
+
 public class Reservation {
     //TODO: Reservation object structure
+    public Flight getFlight() {
+        return null;
+    }
+
+    public void setFlightSeats(SeatDTO[] flightSeats) {
+    }
+
+    public void setFlightPassengers(PassengerRequest[] passengers) {
+    }
 }
