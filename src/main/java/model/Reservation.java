dc9163b08add4351570db68dca196ec1295473fd
Juan Luis Casanova <juanluiscr27@hotmail.com>
2023-04-02 16:00:41 -0400

    Added get method to reservation entity

--- src/main/java/model/Reservation.java
+++ src/main/java/model/Reservation.java
@@ -3,14 +3,28 @@ package model;
 import java.util.List;
 
 public class Reservation {
-    //TODO: Reservation object structure
+    private Flight flight;
+    private SeatDTO[] flightSeats;
+    private PassengerRequest[] passengers;
+
     public Flight getFlight() {
-        return null;
+        return flight;
+    }
+    public void setFlight(Flight flight) {
+        this.flight = flight;
     }
 
+    public SeatDTO[] getFlightSeats() {
+        return flightSeats;
+    }
     public void setFlightSeats(SeatDTO[] flightSeats) {
+        this.flightSeats = flightSeats;
     }
 
+    public PassengerRequest[] getFlightPassengers() {
+        return passengers;
+    }
     public void setFlightPassengers(PassengerRequest[] passengers) {
+        this.passengers = passengers;
     }
 }
